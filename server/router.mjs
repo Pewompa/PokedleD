24294c7b20552bf77bd15879a721cf4397f882ca
Pewompa <bernatpavon@gmail.com>
2023-11-08 17:14:46 +0100

    server out

--- server/router.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getPokemon, postNewPokemon } from './controllers.mjs';
-const pokemonRouter = express.Router();
-
-pokemonRouter.get('/pokemon/:id', getPokemon);
-
-pokemonRouter.post('/pokemon/post', postNewPokemon);
-
-export { pokemonRouter };
