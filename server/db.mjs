bdaa513255319d1cefc2119c8abce106f162366b
Pewompa <bernatpavon@gmail.com>
2023-11-08 11:27:01 +0100

    edit db file

--- server/db.mjs
+++ server/db.mjs
@@ -2,10 +2,10 @@ import { connect } from 'mongoose';
 import { config } from 'dotenv';
 
 config();
-const uri = process.env.MONGODB_URI;
+// const uri = process.env.MONGODB_URI;
 // const uri =
 //   'mongodb+srv://bernatpavon:ioEQ9sz8ZJihkLR5@cluster0.retwlmx.mongodb.net/?retryWrites=true&w=majority';
-// const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 export const connectDB = async () => {
   try {
     await connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
